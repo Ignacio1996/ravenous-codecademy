8ebbb1807ef23d3e670648e2621296066a4ba6eb
Ignacio1996 <in-aguirre@hotmail.com>
2018-09-15 12:31:38 -0400

    latest changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import BusinessList from './components/BusinessList/BusinessList';
 import Business from './components/Business/Business';
 import SearchBar from './components/SearchBar/SearchBar';
 
+var business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+var businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+
+]
 
 class App extends Component {
   render() {
@@ -12,7 +33,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses} />
       </div>      
     );
   }
